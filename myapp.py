348768f60376617777ff76c23c9a0705d8d52168
Matthieu.Rainteau <matthieu.rainteau@sanofi.com>
2022-03-24 17:50:07 +0100

    [ADD] fix load header

--- myapp.py
+++ myapp.py
@@ -10,10 +10,9 @@ def load_data(nrows):
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     return data
-
-# Create a text element and let the reader know the data is loading.
-data_load_state.text("Done! (using st.cache)")
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+    # Create a text element and let the reader know the data is loading.
+    data_load_state.text("Done! (using st.cache)")
+    # Load 10,000 rows of data into the dataframe.
+    data = load_data(10000)
+    # Notify the reader that the data was successfully loaded.
+    data_load_state.text('Loading data...done!')
