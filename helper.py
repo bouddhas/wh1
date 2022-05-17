3bdd5b799a5a549a330578fed16539055737395e
bouddhas <58512932+bouddhas@users.noreply.github.com>
2022-05-17 12:02:18 +0200

    Delete helper.py
    deprecated

--- helper.py
+++
@@ -1,9 +0,0 @@
-import requests
-import pandas as pd
-
-data = pd.read_csv('new_data.csv')
-url = 'http://127.0.0.1:5000/api'
-#r = requests.post(url,data)
-r = requests.post(url,json=(data.loc[0].to_json()))
-
-print(r.json())
